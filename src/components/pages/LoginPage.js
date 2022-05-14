4178a70d2e1c272cf2d18cc73336c4fd78dcf623
ilketorun <ilketorun97@gmail.com>
2022-05-14 15:05:55 +0300

    Pictures and CSS animations Total of 3 Pictures and their and CSS animations are implemented

--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -2,17 +2,25 @@ import AuthForm from "../Auth/AuthForm";
 import classes from "./LoginPage.module.css";
 import wholePicture from "../Images/LoginPage/wholePicture.png";
 import bonusKafa from "../Images/LoginPage/bonusKafa.png";
+import loftiProfile from "../Images/LoginPage/loftiProfile.png";
+import lilMessages from "../Images/LoginPage/lilMessages-2.jpg";
 
 const LoginPage = () => {
   return (
     <div className={classes.container}>
       <div className={classes.picturesItems}>
-        <img src={wholePicture} alt="Discover Page" width={381} />
+        <img src={wholePicture} alt="Discover Page" width={483} />
         <img
-          src={bonusKafa}
+          src={loftiProfile}
           alt="Profile Page"
-          className={classes.innerPicture}
-          width={249}
+          className={classes.innerPictureLofti}
+          width={316}
+        />
+        <img
+          src={lilMessages}
+          alt="Profile Page"
+          className={classes.innerPictureLilMessage}
+          width={316}
         />
       </div>
       {/* <AuthForm /> */}
